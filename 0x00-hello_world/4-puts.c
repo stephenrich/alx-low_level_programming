5787368b9cd8a8a79fd69c7cfae2dcb6c30050ba
stephenrich <stephenrichdace.com>
2022-09-12 15:10:09 -0700

    now

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- Serves as the entry point for program
+ *
+ * Return:By default returns zero if no error otherwise returns non-zero value
+ */
+int main(void)
+{
+	puts("\"Programming like building a nultilingual puzzle");
+	return (0)
+}
