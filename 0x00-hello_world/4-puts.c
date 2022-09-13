ee0384245f48303e02211a64d7cde7d05ff46611
stephenrich <stephenrichdace.com>
2022-09-12 19:24:13 -0700

    man

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,9 @@
 #include <stdio.h>
 
 /**
- *
- *main - print the string in the put function
- *
- * Description: using the main function
- * this program prints "programming is like multilingual puzzle
- * Return: 0
- * /
- Int main(void)
-
- {  
- 
-   puts(“\“Programming is like bulding a multilingual puzzle”);
-   return(0);
- }
-
-
-
+* main - print the string in the put funtion
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
