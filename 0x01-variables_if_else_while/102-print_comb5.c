a139c18a4e1a1f97605880f4e95cc4cf24ee858f
stephenrich <stephenrichdace.com>
2022-09-25 08:07:25 -0700

    make

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <time.h>
 
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-
 	int i, j;
-
 	int a, b, c, d;
 
 	for (i = 0; i < 100; i++)
 	{
+
 		a = i / 10; /* doubles fnum */
 		b = i % 10; /* singles fnum */
 
@@ -25,7 +21,6 @@ int main(void)
 		{
 
 			c = j / 10; /* doubles snum */
-
 			d = j % 10; /* singles snum */
 
 			if (a < c || (a == c && b < d))
@@ -35,20 +30,16 @@ int main(void)
 				putchar(b + '0');
 				putchar(32);
 				putchar(c + '0');
+				putchar(d + '0');
 
 				if (!(a == 9 && b == 8))
-
+				{
 					putchar(44);
 					putchar(32);
-
 				}
-
 			}
-
 		}
-
 	}
 	putchar(10);
 	return (0);
-
 }
