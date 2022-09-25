3da2cf6d48fc67d3a911693814c8889e5066acbf
stephenrich <stephenrichdace.com>
2022-09-25 07:39:36 -0700

    man down

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,67 +5,40 @@
 #include <time.h>
 
 /**
-
  * main - Entry point
-
- *
-
  * Return: Always 0 (Success)
-
  */
 
 int main(void)
-
 {
 
 	int i, j;
 
 	int a, b, c, d;
 
-
-
 	for (i = 0; i < 100; i++)
-
 	{
-
 		a = i / 10; /* doubles fnum */
-
 		b = i % 10; /* singles fnum */
 
-
-
 		for (j = 0; j < 100; j++)
-
 		{
 
 			c = j / 10; /* doubles snum */
 
 			d = j % 10; /* singles snum */
 
-
-
 			if (a < c || (a == c && b < d))
-
 			{
 
 				putchar(a + '0');
-
 				putchar(b + '0');
-
 				putchar(32);
-
 				putchar(c + '0');
-
-				putchar(d + '0');
-
-
 
 				if (!(a == 9 && b == 8))
 
-				{
-
 					putchar(44);
-
 					putchar(32);
 
 				}
@@ -75,9 +48,7 @@ int main(void)
 		}
 
 	}
-
 	putchar(10);
-
 	return (0);
 
 }
