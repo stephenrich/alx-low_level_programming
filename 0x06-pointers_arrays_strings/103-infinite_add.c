966724d6a80f27cd7779b2c8959b3e88f8fddbab
stephenrich <stephenrichdace.com>
2022-09-25 21:27:12 -0700

    green

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,33 +1,33 @@
 #include "main.h"
 /**
-* infinite_add - adds two numbers.
-* @n1: First number
-* @n2: Second number
-* @r: result buffer
-* @size_r: result buffer size
-* Return: 0 if the result can not be stored in r,
-* otherwise a pointer to the result
-*/
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @r: buffer for result
+ * @size_r: buffer size
+ * Return: address of r or 0
+ */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int i, j, k, l, m, n;
-for (i = 0; n1[i]; i++); i 
-	for (j = 0; n2[j]; j++);
+
+	for (i = 0; n1[i]; i++)
+		;
+
+	for (j = 0; n2[j]; j++)
+		;
 
 	if (i > size_r || j > size_r)
-
 		return (0);
 	m = 0;
-
 	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 	{
 		n = m;
+		if (i >= 0)
 
-		if (i >= 0
 			n += n1[i] - '0';
 		if (j >= 0)
 			n += n2[j] - '0';
-		if (i < 0 && j < 0 && n == 0i)
+		if (i < 0 && j < 0 && n == 0)
 		{
 			break;
 
@@ -35,7 +35,9 @@ for (i = 0; n1[i]; i++); i
 		m = n / 10;
 		r[k] = n % 10 + '0';
 	}
+
 	r[k] = '\0';
+
 	if (i >= 0 || j >= 0 || m)
 		return (0);
 	for (k -= 1, l = 0; l < k; k--, l++)
