5f084fe15558c0dacb0a0533df9af0b218a04c9e
stephenrich <stephenrichdace@gmail.com>
2022-12-20 06:19:04 -0800

    reset98

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,4 @@
 #ifndef MAIN_H
-
 #define MAIN_H
 
 int _putchar(char c);
@@ -17,10 +16,6 @@ void print_rev(char *s);
 void rev_string(char *s);
 
 void puts2(char *str);
-
-void puts_half(char *str);
-
-void print_array(int *a, int n);
 
 char *_strcpy(char *dest, char *src);
 
