52cb006fb3495a79835e23b090a6403802a873d3
stephenrich <stephenrichdace@gmail.com>
2022-12-20 07:45:14 -0800

    task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,11 @@
 #include "main.h"
+/**
+ * *  reset_to_98 - takes the pointer and updates the value
+ * *  @n: int to check
+ * *  Return: 0 is success
+ * */
 
- /**
- * reset_to_98 - update the value of a var using points
- * @n:int to update
- * Return:0
- */
-
-void reset_to_98(int *n);
-
+void reset_to_98(int *n)
 {
-	*n = 98;
-	
-
+		*n = 98;
 }
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * * swap_int - swaps the value of int a and int b
- * * Owned by Bwave/Bright
+ * * swap_int - swaps the value of int a and int bt
  * * @a: first int
  * * @b: second int
  * * Return 0
