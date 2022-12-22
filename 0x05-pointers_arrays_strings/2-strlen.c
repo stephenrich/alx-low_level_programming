6a433fb3e264630e15fcbce549fee24ef4e98622
stephenrich <stephenrichdace@gmail.com>
2022-12-21 17:15:10 -0800

    string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,16 +5,13 @@
  * Return:0
  */
 
- int _strlen(char *s)
+
+	int _strlen(char *s)
 {
-  
-int a = 0; /*start the count from 0*/
+int a = 0;	/*start the count from 0*/
 
 	for (; *s++;)
-
 		a++;
 
 	return (a);
-
 }
-	
