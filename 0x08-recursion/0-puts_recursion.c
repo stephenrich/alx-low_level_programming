3048f05300b9a3060d9113653800563b6a1cb94b
stephenrich <stephenrichdace2gmail.com>
2023-09-13 17:27:16 -0700

    str

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+void _puts_recursion(char *s) {
+    if (*s == '\0') {
+
+    putchar('\n'); 
+
+        return;
+    }
+
+    putchar(*s);
+    _puts_recursion(s + 1);
+    }
+
+int main() {
+    char str[] = "Hello, World!";
+    _puts_recursion(str);
+    return 0;
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,6 @@
+
+		RECURSION
+Write a function that prints a string, followed by a new line.
+
+Prototype: void _puts_recursion(char *s);
+FYI: The standard library provides a similar function: puts. Run man puts to learn more.
